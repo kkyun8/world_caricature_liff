97f8d22c992fc4778fff1a8d31f7ac8237ef845a
kkyun8 <kim23302@icloud.com>
2020-01-06 23:02:30 +0900

    test

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,6 +1,7 @@
 window.onload = function() {
+    // require('dotenv').config();
     const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1653636038-PXvldDNv";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
